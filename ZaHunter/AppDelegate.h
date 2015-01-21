9c6a0e6ee8dfb6c38ce5aa8be8b4bd295386d0df
Tewodros Wondimu <tewodros.wr@gmail.com>
2015-01-21 13:47:16 -0800

    Initial Commit

---
+++ ZaHunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZaHunter
+//
+//  Created by Tewodros Wondimu on 1/21/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZaHunter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZaHunter
+//
+//  Created by Tewodros Wondimu on 1/21/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
